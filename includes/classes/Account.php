d1ed2fa244a409fd6a4492c5f0cf08a02db6502c
unknown <purvi.harniya@gmail.com>
2020-09-23 21:34:40 +0530

    sign up val 3

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -28,11 +28,23 @@ class Account{
     }
 
     private function validatePasswords($pw1,$pw2){
+        if(!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $pw1)){
+            array_push($this->errorArray, "Password should have 8-20 characters, atleast one Uppercase letter, atleast one number and atleast one special character!");
+            return;
+        }
+
+        if($pw1 != $pw2){
+            array_push($this->errorArray, "Your passwords don't match.");
+            return;
+        }
 
     }
 
     private function validateProfileName($pn){
-
+        if(strlen($pn) >25 || strlen($pn) < 3 ){
+            array_push($this->errorArray, "Profile name should must be between 3 and 25 characters.");
+            return;
+        }
     }
 
     private function validateContact($no){
