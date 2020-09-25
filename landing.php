145016644a714edead126dec2cb199ad057d1f6e
unknown <purvi.harniya@gmail.com>
2020-09-25 15:50:02 +0530

    music bar desktop

--- landing.php
+++ landing.php
@@ -26,12 +26,24 @@
             <div class="page-container">
             </div>
         </div>
-        <div id="nowPlayingBarContainer" style="height:150px">
-            <div class="row nowPlayingBar">
-                <div class="col-12 col-md-3">
-                    img 
+        <div id="nowPlayingBarContainer">
+            <div class="row py-4 nowPlayingBar">
+
+                <div class="col-12 col-md-3 bar-left content py-1">
+                    <span class="albumlink">
+                        <img src="assets/images/albumimages/album1.jpg" class="albumArtwork">
+                    </span>
+                    <div class="track-info">
+                        <span class="trackname">
+                            <span>Song Name </span>
+                        </span>
+                        <span class="artistname">
+                            <span>Artist Name </span>
+                        </span>
+                    </div>
                 </div> 
-                <div class="col-12 col-md-6 py-3 px-5 d-flex flex-column justify-content-center">
+
+                <div class="col-12 col-md-6 py-1 px-5 d-flex flex-column content">
                     <div class="controlButton-container text-center">
                         <button class="controlButton shuffleButton" title="Shuffle button"> 
                             <img src="assets/images/icons/shuffle.png" alt="shuffle" class="shufflebt-i">
@@ -52,7 +64,7 @@
                             <img src="assets/images/icons/repeat.png" alt="repeat" class="repeatbt-i">
                         </button>
                     </div>
-                    <div class="playBar-container d-flex justify-content-center">
+                    <div class="playBar-container d-flex justify-content-center pt-2">
                         <span class="progressTime current pr-3"> 0.00 </span> 
 
                         <div class="progressbar">
@@ -64,8 +76,21 @@
                         <span class="progressTime remaining pl-3"> 0.00 </span> 
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
-                    options
+
+                <div class="col-12 col-md-3 d-none d-md-block content align-self-center">
+                    <div class="volume-bar d-flex justify-content-center">
+
+                        <button class='controlButton volumeButton' title="volume button">
+                            <img src="assets/images/icons/volume.png" alt="volume" class="volumebt-i">
+                        </button>
+
+                        <div class="progressbar">
+                            <div class="progressbar-bg">
+                                <div class="progressprogress"></div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
